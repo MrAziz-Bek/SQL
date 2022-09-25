78cea675cf92590d89ceb89cfeae3a966ecb3105
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-09-25 10:08:30 +0500

    Inserting a row

---
+++ MySQL/Inserting-Updating-Deleting_Data/Inserting-a-Row.sql
@@ -0,0 +1,14 @@
+INSERT INTO customers (
+    first_name,
+    last_name,
+    birth_date,
+    address,
+    city,
+    state)
+VALUES (
+    'John',
+    'Smith',
+    '1990-01-01',
+    'address',
+    'city',
+    'CA');
