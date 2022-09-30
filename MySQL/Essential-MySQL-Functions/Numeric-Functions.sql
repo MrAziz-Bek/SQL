d49f4bb3eb51897b053646e0c7b0aed5870e80cf
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-09-30 06:51:10 +0500

    Numeric functions

---
+++ MySQL/Essential-MySQL-Functions/Numeric-Functions.sql
@@ -0,0 +1,13 @@
+SELECT RAND();
+SELECT ABS(-5.2);
+SELECT FLOOR(5.2);
+SELECT CEILING(5.2);
+SELECT TRUNCATE(5.678, 2);
+SELECT ROUND(5.678, 2);
+
+-- RAND() is used to a return random floating-point value V in the range 0 <= V < 1.0.
+-- ABS is used to return the absolute value of a specified number.
+-- FLOOR returns the largest integer value that is equal to or less than a number:
+-- CEILING returns the smallest integer value that is greater than or equal to a number:
+-- TRUNCATE. Truncation means to chop off the decimal portion of a number
+-- ROUND function returns a number rounded to a certain number of decimal places.
