6e3ca83dc44ffb65fd60ca0b047e545e839bd698
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-10-02 21:14:10 +0500

    parameters with default values

---
+++ MySQL/Stored-Procedures/parameters-with-default-values-exercise.sql
@@ -0,0 +1,21 @@
+CALL get_clients_by_state(NULL);
+
+DROP PROCEDURE IF EXISTS get_clients_by_state;
+
+DELIMITER $$
+CREATE PROCEDURE get_clients_by_state
+(
+    state CHAR(2)
+)
+BEGIN
+    -- IF state IS NULL THEN
+    --     SELECT * FROM clients;
+    -- ELSE
+    --     SELECT * FROM clients c
+    --     WHERE c.state = state;
+    -- END IF;
+    SELECT * FROM clients c
+    WHERE c.state = IFNULL(state, c.state);
+END$$
+
+DELIMITER ;
